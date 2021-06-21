483c170b1a6b68b622f11fe3eb74cc343a3ca64f
Dheeraj Bharat Dangi <47323944+ddangi66@users.noreply.github.com>
2021-06-21 11:48:31 +0530

    Update matrix_multiplication.c

--- matrix_multiplication.c
+++ matrix_multiplication.c
@@ -1,1 +1,32 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+void print_matrix(int,int);
+
+void accept_matrix(int r,int c)
+{
+  int i,j,a[r][c];
+  for(i=0;i<r;i++)
+  {
+    for(j=0;j<c;j++)
+    {
+      printf("\nA[%d][%d]",i,j);
+      scanf("%d",&a[i][j]);
+    }
+  }
+  
+}
+
+void main()
+{
+  int r,c,r1,c1;
+  printf("\nEnter The Number Of Rows And Columns Of First Matrix\n");
+  scanf("%d%d",&r,&c);
+  accept_matrix(r,c);
+  print_matrix;
+  printf("\nEnter The Number Of Rows And Columns Of Second Matrix\n");
+  scanf("%d%d",&r1,&c1);
+  
+  
+}
+
