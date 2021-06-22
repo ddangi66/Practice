dc8088aca619ce05c3bbb1f0789c698323226188
Dheeraj Bharat Dangi <47323944+ddangi66@users.noreply.github.com>
2021-06-22 09:55:56 +0530

    Update matrix_multiplication.c

--- matrix_multiplication.c
+++ matrix_multiplication.c
@@ -1,75 +1,65 @@
-#include<stdio.h>
-void multiply(int mat1[12][12],int mat2[12][12],int ,int ,int );
- 
-void main()
+#include <stdio.h>
+#include <stdlib.h>
+
+void input(int m, int n, int a[m][n])
 {
-    int mat1[12][12],mat2[12][12];
-    int i,j,k,m,n,p;
-    printf("Enter the number of rows and columns for 1st matrix\n");
-    scanf("%d%d",&m,&n);
-    printf("Enter the elements of the 1st matrix\n");
-    for(i=0;i<m;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            scanf("%d",&mat1[i][j]);
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d, %d : ", i, j);
+            scanf("%d", &a[i][j]);
         }
     }
-    //no of col of 1st mat = no of rows of 2nd mat
-    printf("Enter the number of columns for 2nd matrix\n"); 
-    scanf("%d",&p);
-    printf("Enter the elements of the 2nd matrix\n");
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<p;j++)
-        {
-            scanf("%d",&mat2[i][j]);
-        }
-    }
-    
-    printf("The 1st matrix\n");
-    for(i=0;i<m;i++)
-    {
-        for(j=0;j<n;j++)
-        {
-            printf("%d\t",mat1[i][j]);
-        }
-        printf("\n");
-    }
-    printf("The 2nd matrix\n");
-    for(i=0;i<n;i++)
-    {
-        for(j=0;j<p;j++)
-        {
-            printf("%d\t",mat2[i][j]);
-        }
-        printf("\n");
-    }
-    multiply(mat1,mat2,m,n,p);
 }
- 
-void multiply(int mat1[12][12],int mat2[12][12],int m,int n,int p)
+
+void print(int m, int n, int a[m][n])
 {
-    int mul[12][12],i,j,k;
-    for(i=0;i<m;i++)
-    {
-        for(j=0;j<p;j++)
-        {
-            mul[i][j]=0;
-            for(k=0;k<n;k++)
-            {
-                mul[i][j]=mul[i][j]+mat1[i][k]*mat2[k][j];
+    int i, j;
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%3d ", a[i][j]);
+        }
+        printf("\n");
+    }   
+}
+
+void multiply(int m, int n, int p, int a[m][n], int b[n][p], int c[m][p])
+{
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            c[i][j] = 0;
+            for (int k = 0; k < n; k++) {
+                c[i][j] += a[i][k] * b[k][j];
             }
         }
     }
- 
-    printf("The resultant matrix formed on multiplying the two matrices\n");
-    for(i=0;i<m;i++)
-    {
-        for(j=0;j<p;j++)
-        {
-            printf("%d\t",mul[i][j]);
-        }
-        printf("\n");
+}
+
+int main()
+{
+    int r1, c1, r2, c2;
+    printf("Row and column for matrix #1 :\n");
+    scanf("%d %d", &r1, &c1);
+    printf("Row and column for matrix #2 :\n");
+    scanf("%d %d", &r2, &c2);
+
+    if (r2 != c1) {
+        printf("Matrices with the given order cannot be multiplied with each other..!\n");
+        exit(EXIT_FAILURE);
     }
+
+    int mat1[r1][c1], mat2[r2][c2], ans[r1][c2];
+    printf("Enter elements of the first matrix.\n");
+    input(r1, c1, mat1);
+    printf("The elements of the first matrix are :\n");
+    print(r1, c1, mat1);
+    printf("Enter elements of the second matrix.\n");
+    input(r2, c2, mat2);
+    printf("The elements of the second matrix are :\n");
+    print(r2, c2, mat2);
+
+    multiply(r1, r2, c2, mat1, mat2, ans);
+    printf("The product is :\n");
+    print(r1, c2, ans);
+
+    return EXIT_SUCCESS;
 }
