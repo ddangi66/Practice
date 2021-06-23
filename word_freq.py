38556ae19e577d475298ee9fed5ef831d8b1c36d
Dheeraj Bharat Dangi <47323944+ddangi66@users.noreply.github.com>
2021-06-23 11:53:12 +0530

    Create word_freq.py

---
+++ word_freq.py
@@ -0,0 +1,9 @@
+input_line = input("Enter a string : ")
+
+words_dict = {}
+
+for word in input_line.split():
+    words_dict[word] = words_dict.get(word,0) + 1
+
+for key in sorted(words_dict):
+  print("{} : {}".format(key,words_dict[key]))
